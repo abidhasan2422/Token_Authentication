0b763f02d4640cad601fe0e1797dcedf3ee83342
Abid Hasan <likhon2422@gmail.com>
2026-03-09 23:15:22 +0600

    version-1.0

---
+++ api/views.py
@@ -0,0 +1,32 @@
+
+from .models import Student
+from .serializers import StudentSerializer
+from rest_framework.generics import GenericAPIView
+from rest_framework.mixins import ListModelMixin,CreateModelMixin,RetrieveModelMixin,DestroyModelMixin,UpdateModelMixin
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly,AllowAny,IsAuthenticated,IsAdminUser
+
+class LCStudentAPI(GenericAPIView,ListModelMixin,CreateModelMixin):
+    queryset= Student.objects.all()
+    serializer_class = StudentSerializer
+    authentication_classes=[TokenAuthentication]
+    permission_classes=[IsAuthenticated]
+    def get(self,request,*args, **kwargs):
+        return self.list(request, *args,**kwargs)
+    def post(self,request,*args,**kwargs):
+        return self.create(request,*args, **kwargs)
+
+class UDRStudentAPI(GenericAPIView,UpdateModelMixin,RetrieveModelMixin,DestroyModelMixin):
+     queryset = Student.objects.all()
+     serializer_class= StudentSerializer
+     authentication_classes=[TokenAuthentication]
+     permission_classes=[]
+
+     def get(self,request,*args, **kwargs):
+         return self.retrieve(request,*args, **kwargs)
+     def put(self,request,*args, **kwargs):
+         return self.update(request,*args, **kwargs)
+     def delete(self,request,*args, **kwargs):
+         return self.destroy(request, *args, **kwargs)
+    
+
---
+++ token_auth/urls.py
@@ -0,0 +1,29 @@
+"""
+URL configuration for token_auth project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/6.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+from django.contrib import admin
+from django.urls import path,include
+from api import views
+from rest_framework.authtoken.views import obtain_auth_token
+
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("studentapi/", views.LCStudentAPI.as_view()),
+    path("studentapi/<int:pk>/", views.UDRStudentAPI.as_view()),
+    path('gettoken/', obtain_auth_token),
+]
